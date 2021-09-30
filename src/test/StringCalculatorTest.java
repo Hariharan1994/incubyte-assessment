d02b38d59882458ca4c6c482e4c55f3c64c8036d
HARIHARAN <hariharan.m@vaken.cloud>
2021-09-30 20:42:47 +0530

    refs #task_01 - empty, single and two number inputs

---
+++ src/test/StringCalculatorTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hari.tdd.assessment.StringCalculator;
+
+/**
+ * This is used to execute the JUNIT.
+ * 
+ * @author HARIHARAN MANI
+ * @since 30-09-2021
+ */
+public class StringCalculatorTest {
+
+	private StringCalculator stringCalculator;
+
+	@Before
+	public void initialize() {
+		stringCalculator = new StringCalculator();
+	}
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(stringCalculator.add(""), 0);
+	}
+
+	@Test
+	public void testSingleNumber() {
+		assertEquals(stringCalculator.add("10"), 10);
+	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(stringCalculator.add("10,20"), 30);
+	}
+
+}
